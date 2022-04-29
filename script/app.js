4b2d591b46662cffc59c76da70283e40ceef4b93
Pierre <ways59.wattelle@gmail.com>
2022-04-29 19:37:21 +0200

    2nd commit

--- script/app.js
+++
@@ -1,8 +0,0 @@
-function copy(str) {
-    const element = document.createElement('textarea')
-    element.value = str;
-    document.body.appendChild(element);
-    element.select();
-    document.execCommand('copy');
-    document.body.removeChild(element);    
-}
